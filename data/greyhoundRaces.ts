7fdd232051bddfc15fd76515a81b56f785e3609e
Darren Jordan <bingopilingo@gmail.com>
2025-05-30 01:57:58 +0100

    Added greyhound races data and page update

---
+++ data/greyhoundRaces.ts
@@ -0,0 +1,76 @@
+export const races = {
+  Today: [
+    {
+      track: "Shelbourne Park",
+      countryCode: "ie",
+      raceTime: "19:45",
+      raceName: "Open 480m Final",
+      runners: ["Fast Jet", "Silver Fox", "Rapid Storm", "Moonlight Dash", "Blazing Star", "Dark Whisper"],
+    },
+    {
+      track: "Harlow",
+      countryCode: "gb",
+      raceTime: "20:15",
+      raceName: "Sprint 400m Semi",
+      runners: ["Red Rocket", "Blue Flame", "Grey Tornado", "White Comet", "Shadow Dash", "Blackout"],
+    },
+    {
+      track: "Nottingham",
+      countryCode: "gb",
+      raceTime: "18:45",
+      raceName: "Evening Dash",
+      runners: ["Jet Stream", "Whirlwind", "Black Viper", "Night Howl", "Golden Arrow", "Stealth Mode"],
+    },
+    {
+      track: "Dundalk",
+      countryCode: "ie",
+      raceTime: "21:00",
+      raceName: "Late Night Sprint",
+      runners: ["Storm Cloud", "Blazing Trail", "Red Zone", "Silver Strike", "Phantom Jet", "Lunar Light"],
+    },
+    {
+      track: "Oxford",
+      countryCode: "gb",
+      raceTime: "22:10",
+      raceName: "Final Burst",
+      runners: ["Heat Wave", "Black Steel", "Crimson Dash", "Gold Rush", "Turbo Jet", "Midnight Mist"],
+    }
+  ],
+  Tomorrow: [
+    {
+      track: "Limerick",
+      countryCode: "ie",
+      raceTime: "18:30",
+      raceName: "Novice 525m",
+      runners: ["Lucky Lad", "Tiny Dancer", "Electric Dash", "Nocturne", "Star Blazer", "Swift Breeze"],
+    },
+    {
+      track: "Romford",
+      countryCode: "gb",
+      raceTime: "21:00",
+      raceName: "Champion Stakes",
+      runners: ["Fireball", "Steel Runner", "Grey Phantom", "Blitz", "Wild Fury", "Night Runner"],
+    },
+    {
+      track: "Clonmel",
+      countryCode: "ie",
+      raceTime: "17:50",
+      raceName: "Twilight Sprint",
+      runners: ["Dash Dream", "Silver Thunder", "Howler", "Fast Spark", "Deep Dive", "Blue Hound"],
+    },
+    {
+      track: "Towcester",
+      countryCode: "gb",
+      raceTime: "19:15",
+      raceName: "Starlight Stakes",
+      runners: ["Flashpoint", "Razor Edge", "Dark Zone", "Solar Flare", "Gale Force", "Venom"],
+    },
+    {
+      track: "Waterford",
+      countryCode: "ie",
+      raceTime: "20:40",
+      raceName: "Evening Classic",
+      runners: ["Night Shift", "Blackout", "Hurricane Jet", "Silent Storm", "Glide Path", "Quick Shot"],
+    }
+  ]
+};
