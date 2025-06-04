1dee9fa00c9466b20250b24c15e62928f8d2532a
Darren Jordan <bingopilingo@gmail.com>
2025-06-04 14:44:47 +0100

    Temporary commit to trigger deployment

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,32 +1,29 @@
-// src/pages/api/auth/[...nextauth].ts
-
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions, User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 const options: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: 'Admin Login',
+      name: 'Credentials',
       credentials: {
         username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
+        password: { label: 'Password', type: 'password' }
       },
-      async authorize(credentials) {
+      async authorize(credentials, req): Promise<User | null> {
         if (
           credentials?.username === process.env.ADMIN_USERNAME &&
           credentials?.password === process.env.ADMIN_PASSWORD
         ) {
-          return { id: 1, name: 'Admin' };
+          return {
+            id: '1', // <-- make sure this is a string, not number
+            name: 'Admin'
+          };
         }
         return null;
-      },
+      }
     }),
   ],
-  session: { strategy: 'jwt' },
-  pages: {
-    signIn: '/admin/login',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+  // ...other NextAuth config
 };
 
 export default NextAuth(options);
