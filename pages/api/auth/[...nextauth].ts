7be231bd5fc466165fc16812054c033d94d9a070
Darren Jordan <bingopilingo@gmail.com>
2025-06-04 15:02:18 +0100

    Fix next-auth config and types

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,29 +1,29 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-const options: NextAuthOptions = {
+export default NextAuth({
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: 'Admin Login',
       credentials: {
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials) {
+     async authorize(credentials) {
+        // Validate against environment variables
         if (
           credentials?.username === process.env.ADMIN_USERNAME &&
           credentials?.password === process.env.ADMIN_PASSWORD
         ) {
-          return {
-            id: '1',    // Must be string
-            name: 'Admin',
-          };
+          return { id: 1, name: 'Admin' };
         }
         return null;
       },
     }),
   ],
-  // You can add additional NextAuth options here, e.g. session, callbacks, etc.
-};
-
-export default NextAuth(options);
+  session: { strategy: 'jwt' },
+  pages: {
+    signIn: '/admin/login', // Optional: you can set your admin login page
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
